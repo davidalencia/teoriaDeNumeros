f92f28ee5ee13f2352bdc7137a8f8f0a3017fade
David Valencia <davidalencia@gmail.com>
2022-11-07 07:56:56 -0600

    congruencias, incluye ejercicios de manera explicita

--- congruencias.py
+++ congruencias.py
@@ -23,11 +23,35 @@ def encuentra_congruencia(cil):
 print(encuentra_congruencia([CI(101, 2, 103)]))
 #print(encuentra_congruencia([CI(1, 5, 7), CI(1, 7, 11), CI(1, 3, 13)]))
 
+
+def _4():
+  print(encuentra_congruencia([CI(8, 14, 24)]))
+  print(encuentra_congruencia([CI(57, 108, 4)]))
+  print(encuentra_congruencia([CI(27, 1, 51)]))
+  print(encuentra_congruencia([CI(980, 1500, 1600)]))
+
+def _5():
+  print(encuentra_congruencia([CI(1, 2, 910), CI(1, 80, 350)]))
+  print(encuentra_congruencia([CI(3, 2, 4), CI(4, 1, 5), CI(6, 3, 5)]))
+  print(encuentra_congruencia([CI(1, 3, 10), CI(1, 8, 15), CI(1, 5, 84)]))
+  print(encuentra_congruencia([CI(1, 5, 7), CI(1, 7, 11), CI(1, 3, 13)]))
+
+
+
+
 def _6a():
+  for x in range(190, 220+1):
+    if CI(1,1,2).resuelve(x) and\
+      CI(1,2,3).resuelve(x-12) and\
+      CI(1,4,7).resuelve(x-36):
+      return x
+
+      
+
+def _6b():
   for x in range(31_500_000, 31_500_500+1):
     if CI(1,7,10).resuelve(x) and\
        CI(1,3,9).resuelve(x) and\
        CI(1,4,7).resuelve(x):
       return x
 
-print(2175%7)
